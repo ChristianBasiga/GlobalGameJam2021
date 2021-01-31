8e55e63250ffa0c4338e284e7708340706d13da2
SKumar74 <sanjk7400@gmail.com>
2021-01-31 11:57:28 -0800

    Scenes and scripts for title and pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+
+    public PlayerController pc;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject UICamera;
+
+    public GameObject PlayerCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+        
+    }
+
+
+    void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        UICamera.SetActive(false);
+        PlayerCamera.SetActive(true);
+        //Time.timeScale = 1;
+        GameisPaused = false;
+    }
+
+
+    void PauseGame()
+    {
+        UICamera.SetActive(true);
+        PlayerCamera.SetActive(false);
+       
+        pauseMenuUI.SetActive(true);
+        //Time.timeScale = 0;
+        GameisPaused = true;
+    }
+
+
+
+}
---
+++ Assets/Scripts/ReturnMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenu : MonoBehaviour
+{
+    public void toMainMenu()
+    {
+        Debug.Log("Attempting to return to the title!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
